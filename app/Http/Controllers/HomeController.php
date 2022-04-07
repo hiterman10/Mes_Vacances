ca964405f2964d2768b0ee340bba006333feb84c
hiterman10 <djiloherman@gmail.com>
2022-04-07 11:55:46 +0200

    mise en place de la page accueil et installation de la libraireie livewire

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Home;
+
+class HomeController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke()
+    {
+        $homes = Home::all();
+        return view('home', compact('homes'));
+    }
+}
